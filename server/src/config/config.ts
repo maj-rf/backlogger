c96d881bb54a3c1cabb64d038a939eb7fa400f24
maj-rf <rmkfermin@gmail.com>
2024-10-21 17:32:39 +0800

    feat: create config files

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -1,4 +1,6 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const PORT = process.env.PORT || '3003';
+export const PORT = Number(process.env.PORT) || 3003;
+export const DEV_DB_URL = process.env.DEV_DB_URL || '';
+export const DB_URL = process.env.DB_URL || '';
