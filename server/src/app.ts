2f34642f9a3dec384a2654d26ee9c64c9d275783
maj-rf <rmkfermin@gmail.com>
2024-10-25 11:05:56 +0800

    create errorHandler middleware

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,8 @@
 import express from 'express';
+import 'express-async-errors';
 import { genreRouter } from './routes/genre.route';
 import { gamesRouter } from './routes/games.route';
+import * as middlewares from './middlewares/middlewares';
 
 const app = express();
 
@@ -9,4 +11,6 @@ app.use(express.json());
 
 app.use('/', genreRouter);
 app.use('/games', gamesRouter);
+
+app.use(middlewares.errorHandler);
 export default app;
